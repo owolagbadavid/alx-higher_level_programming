d8b64407367f92990fbcbafb15113cfd9a70d2e0
David <owolagbadavid@gmail.com>
2023-09-04 12:53:42 +0100

    c fix

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,28 +1,18 @@
 #include "lists.h"
 
-/**
- * check_cycle - fun
- * @head: pointer
- *
- * Return: pointer
- */
 int check_cycle(listint_t *list)
 {
-	listint_t *temp1 = list;
-	listint_t *temp2 = list;
+	listint_t *fast;
+	listint_t *slow;
 
-	if (list == NULL)
-		return (0);
-
-	while (temp1 && temp2 && temp2->next)
+	fast = list;
+	slow = list;
+	while (fast != NULL && fast->next != NULL)
 	{
-		temp2 = temp2->next->next;
-		temp1 = temp1->next;
-		if (temp2 == temp1)
-		{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (fast == slow)
 			return (1);
-		}
 	}
-
 	return (0);
-}
+} 
