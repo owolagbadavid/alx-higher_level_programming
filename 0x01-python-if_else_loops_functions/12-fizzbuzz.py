377f9977b1cc24ffe7be546b6f6bb7de18d3f7c9
David <owolagbadavid@gmail.com>
2023-09-05 18:16:30 +0100

    python fix

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def fizzbuzz():
     for i in range(1, 101):
         if i % 15 == 0:
@@ -7,4 +8,4 @@ def fizzbuzz():
         elif i % 5 == 0:
             print("Buzz", end=" ")
         else:
-            print(f"{i:d}", end = " ")
+            print("{:d}".format(i), end = " ")
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,4 @@
 def print_last_digit(number):
     last = abs(number) % 10
-    print(last, end="")
+    print("{:d}".format(last), end="")
     return last
