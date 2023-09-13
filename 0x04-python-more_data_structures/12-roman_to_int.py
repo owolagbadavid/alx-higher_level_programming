cedfb70491a3acd68138759bfdb29fa750b02010
DAVID <owolagbadavid@gmail.com>
2023-09-13 21:41:55 +0100

    mandat

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+    roman_numeral = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    }
+    total = 0
+    for i in range(len(roman_string)):
+        if i > 0 and roman_numeral[roman_string[i]] >\
+                roman_numeral[roman_string[i - 1]]:
+            total += roman_numeral[roman_string[i]] -\
+                2 * roman_numeral[roman_string[i - 1]]
+        else:
+            total += roman_numeral[roman_string[i]]
+    return total
