924eed1e194eb7aecba45d182af5fb3d8e7e38cf
DAVID <owolagbadavid@gmail.com>
2023-09-25 19:07:09 +0100

    fix 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,6 +8,6 @@ def safe_print_list_integers(my_list=[], x=0):
                 i += 1
         print()
         return i
-    except (TypeError, ValueError, IndexError):
+    except (TypeError, ValueError):
         print()
         return i
