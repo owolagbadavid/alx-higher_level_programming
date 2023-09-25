a6706c311365d7a242bd88474721648244518398
DAVID <owolagbadavid@gmail.com>
2023-09-25 19:00:14 +0100

    mandat

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        i = 0
+        for j in range(x):
+            if type(my_list[j]) is int:
+                print("{:d}".format(my_list[j]), end="")
+                i += 1
+        print()
+        return i
+    except (TypeError, ValueError, IndexError):
+        print()
+        return i
