d14d71cd63179b169e2f3c6860ed2b545c4cef0c
DAVID <owolagbadavid@gmail.com>
2023-10-02 13:56:49 +0100

    fix instance count

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -71,6 +71,7 @@ class Rectangle:
 
     def __del__(self):
         print('Bye rectangle...')
+        Rectangle.number_of_instances -= 1
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
