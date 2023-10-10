c5e1b2fc167e1c81a2497073feb2e2c21ba53cad
DAVID <owolagbadavid@gmail.com>
2023-10-10 19:09:44 +0100

    extra fix

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -3,15 +3,7 @@
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """ Function that appends a new line when a string is found
-
-    Args:
-        filename: filename
-        search_string: string to search
-        new_string: string to append
-
-    """
-
+    """Function that appends a new line when a string is found"""
     res_line = []
     with open(filename, 'r', encoding="utf-8") as f:
         for line in f:
