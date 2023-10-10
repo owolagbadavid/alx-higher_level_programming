852580dc3fb19c96c9c5d9630b55358bef50f102
DAVID <owolagbadavid@gmail.com>
2023-10-10 19:08:55 +0100

    extra fix

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,38 +1,52 @@
 #!/usr/bin/python3
-"""Script that reads stdin line by line and computes metrics"""
 import sys
 
 
-def print_stats(file_size, status_codes):
-    """Function that prints the stats"""
-    print("File size: {}".format(file_size))
+def print_stats():
+    print('File size: {:d}'.format(file_size))
 
     for key, value in sorted(status_codes.items()):
-        if value != 0:
-            print("{}: {}".format(key, value))
+        if value > 0:
+            print('{}: {:d}'.format(key, value))
 
 
-status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
-                "403": 0, "404": 0, "405": 0, "500": 0}
+status_codes = {
+    '200': 0,
+    '301': 0,
+    '400': 0,
+    '401': 0,
+    '403': 0,
+    '404': 0,
+    '405': 0,
+    '500': 0
+}
 
-file_size = 0
 counter = 0
+file_size = 0
 
 try:
     for line in sys.stdin:
-        counter += 1
+        if counter != 0 and counter % 10 == 0:
+            print_stats()
+
         data = line.split()
+
+        try:
+            status = int(data[-2])
+
+            if str(status) in status_codes.keys():
+                status_codes[str(status)] += 1
+        except:
+            pass
+
         try:
             file_size += int(data[-1])
         except:
             pass
-        try:
-            status_codes[data[-2]] += 1
-        except:
-            pass
-        if counter % 10 == 0:
-            print_stats(file_size, status_codes)
-    print_stats(file_size, status_codes)
+
+        counter += 1
+
+    print_stats()
 except KeyboardInterrupt:
-    print_stats(file_size, status_codes)
+    print_stats()
     raise
