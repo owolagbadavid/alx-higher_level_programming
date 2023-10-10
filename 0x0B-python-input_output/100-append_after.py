1a6a1520608822381ba9b94ea785795f6d909c4c
DAVID <owolagbadavid@gmail.com>
2023-10-10 18:46:50 +0100

    extra

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Function that inserts a line of text to a file, after each line containing
+a specific string"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Function that inserts a line of text to a file, after each line
+    containing a specific string"""
+    with open(filename, mode='r+', encoding='utf-8') as f:
+        lines = f.readlines()
+        for i, line in enumerate(lines):
+            if search_string in line:
+                lines.insert(i + 1, new_string)
+        f.seek(0)
+        f.writelines(lines)
---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Script that reads stdin line by line and computes metrics"""
+import sys
+import os.path
+
+
+def print_stats(file_size, status_codes):
+    """Function that prints the stats"""
+    print("File size: {}".format(file_size))
+    for key, value in sorted(status_codes.items()):
+        if value != 0:
+            print("{}: {}".format(key, value))
+
+
+def parse_line(line, file_size, status_codes):
+    """Function that parses a line"""
+    try:
+        line = line.split()
+        file_size += int(line[-1])
+        status_codes[line[-2]] += 1
+    except:
+        pass
+    return file_size, status_codes
+
+def main():
+    """Function that reads stdin line by line and computes metrics"""
+    file_size = 0
+    status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+                    "403": 0, "404": 0, "405": 0, "500": 0}
+    i = 0
+    try:
+        for line in sys.stdin:
+            file_size, status_codes = parse_line(line, file_size, status_codes)
+            i += 1
+            if i % 10 == 0:
+                print_stats(file_size, status_codes)
+    except KeyboardInterrupt:
+        print_stats(file_size, status_codes)
+        raise
+
+if __name__ == "__main__":
+    main()
